a2a81e3c19c9e4b3241002b5565a9ea90b214744
Nikita Bayev <nikita@bayev.kz>
2014-01-14 17:08:47 +0600

    Added pages settings

--- app/index.js
+++ app/index.js
@@ -21,39 +21,49 @@ SimpleGenerator.prototype.askFor = function askFor() {
 		name: 'projectName',
 		message: 'What do you want to call your project?'
 	}, {
+		name: 'pages',
+		message: 'What pages you will use in your project? (separated by spaces) \n For example: blog news-page info-page; \n Pages:'
+	}, {
 		name: 'blocks',
 		message: 'What blocks you will use in your project? (separated by spaces) \n For example: search button common menu; \n Blocks:'
 	}];
 	this.prompt(prompts, function (props) {
 		this.projectName = props.projectName;
+		this.pages = props.pages.split(' ');
 		this.blocks = props.blocks.split(' ');
 		cb();
 	}.bind(this));
 };
 
 SimpleGenerator.prototype.app = function app() {
+	this.mkdir('static');
 	this.mkdir('js');
 	this.mkdir('css');
 	this.mkdir('css/blocks');
 	this.mkdir('img');
 	
-	this.write('index.html', 'index.html');
+	this.write('static/index.html', 'index.html');
 	
 	var blocks = [];
 	
-	for (var i in this.blocks) {
-		this.write('css/blocks/_' + this.blocks[i] + '.css', '/* Block: ' + this.blocks[i] + '*/');
-		blocks.push('@import url(css/blocks/_' + this.blocks[i] + '.css);\n');
+	for (var p in this.pages) {
+		this.write('static/' + this.pages[p] + '.html', '');
+	}
+	
+	for (var b in this.blocks) {
+		this.write('css/blocks/_' + this.blocks[b] + '.css', '/* Block: ' + this.blocks[b] + '*/');
+		blocks.push('@import url(css/blocks/_' + this.blocks[b] + '.css);\n');
 	}
 	
 	blocks = blocks.toString().replace(/,/g, '');
-	this.write('css/styles.css', blocks);
 	
-	this.template('_Gruntfile.js', 'Gruntfile.js');
-	this.template('_package.json', 'package.json');
-	this.template('_bower.json', 'bower.json');
+	this.write('css/styles.css', blocks);
 };
 
+SimpleGenerator.prototype.templatefiles = function templatefiles() {
+	this.template('_Gruntfile.js', 'Gruntfile.js');
+	this.template('_package.json', 'package.json');
+};
 
 SimpleGenerator.prototype.projectfiles = function projectfiles() {
 	this.copy('editorconfig', '.editorconfig');
